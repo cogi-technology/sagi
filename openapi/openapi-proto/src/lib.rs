71ba143a4bfd62fad61559926cdbb183cd4a524b
dinhnq-gihot <dinh.nguyen@gihot.vn>
2024-07-24 18:59:13 +0700

    chore: fix proto module

--- openapi/openapi-proto/src/lib.rs
+++ openapi/openapi-proto/src/lib.rs
@@ -1,28 +1,11 @@
-pub mod openapi_services {
-    pub mod erc20_service {
-        tonic::include_proto!("erc20");
-    }
-
-    pub mod erc721_service {
-        tonic::include_proto!("erc721");
-    }
-
-    pub mod erc404_service {
-        tonic::include_proto!("erc404");
-    }
+pub mod erc20_service {
+    tonic::include_proto!("erc20");
 }
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub mod erc721_service {
+    tonic::include_proto!("erc721");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod erc404_service {
+    tonic::include_proto!("erc404");
 }
