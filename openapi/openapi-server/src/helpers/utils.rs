9f063980cc558b12d9b4ad20abc8b37bd1c5d56e
dinhnq-gihot <dinh.nguyen@gihot.vn>
2024-08-07 00:29:56 +0700

    chore: modify request get authorization data

--- openapi/openapi-server/src/helpers/utils.rs
+++ openapi/openapi-server/src/helpers/utils.rs
@@ -16,12 +16,10 @@ use zion_aa::{
         operator::Operator,
         wallet::ContractWallet,
     },
-    types::{jwt::JWTOptions, login::LoginData},
+    types::{jwt::JWTOptions, request::AuthorizationData},
 };
 
 use crate::services::zionauthorization::get_data_request_for_zion_logic;
-
-use super::into::{proto_proofpoint_from, sdk_proofpoint_from};
 
 pub type Result<T> = std::result::Result<T, tonic::Status>;
 
@@ -107,7 +105,7 @@ pub async fn init_contract_wallet(
     //     },
     //     token_data,
     // ) = get_data_request_for_zion_logic(header_metadata).await?;
-    let token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImpOVHRoNU80WHFyT0NuR1BkeGtEME5pOCIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjI5MzM4MzMsImV4cCI6MTcyMzAyMDIzMywibmJmIjoxNzIyOTMzODMzLCJpc3MiOiJodHRwczovL2lkLnRlbGVncmFtLm9yZyIsInN1YiI6IjU3NDA4NDczOTkiLCJhdWQiOiI3MTA5NzQwNDgyIn0.Sog1eSp9MKaPxau9RV-mK9o89ZiCEe8T4qN6A4M6u87MRgRlOgJRau_LYzZ2Pnotcviwfo8L6CRj-7hEI-DDGm3h3cyQ4bT48PBciTzWGrlmGmwp4gRRZgfJyWpqYDUJlue_99ermhS3KMUPM2v5mM99Ya44pPFBEV-FVXXc2CSj_994mYfWT1lhKiVKeTtEzxDt7VDv0M25JksLcEA9-SVfyeM0ehQytGFjGrrawQjVCSqgFN8RwT5RyvFdcn7OwWPSoechbrgjQTNYbb3eiDLQjInrmtRZeslmFO5rMd3VCr_t3dQTF_IInwIHBdbBtHiklozq8aJDTwDnL3w75w";
+    let token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImpOVHRoNU80WHFyT0NuR1BkeGtEME5pOCIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MjI5NjE4OTQsImV4cCI6MTcyMzA0ODI5NCwibmJmIjoxNzIyOTYxODk1LCJpc3MiOiJodHRwczovL2lkLnRlbGVncmFtLm9yZyIsInN1YiI6IjU3NDA4NDczOTkiLCJhdWQiOiI3MTA5NzQwNDgyIn0.igL0Z8LbR3awnox4slTvbBsqnnWJyUQ4p1qvcJYwnkdO5py4W3mTuaFzx0xw8ByECIuK0juU9uQIg7xZQZV_nUXk5M_S8snMvSo2nn3Q_TTqbgDHGqtATLPMEkKAqnVHQQooJ_CkqO97AHgoQCTWkgZGoOhDg773I-CO1RQ3xUI7K6PP96X2CNA02Ez4JrRNklaJ44eR46I7sgmQWMzQzFx7iy6niSNXue1rZIWZsjws8NUBYZuTn_b24V56RCZw__j8-9NAmqFv2vitIwDiNMHk96hJDllSsJXOmoYukuIR0467qlTRJrJqW5_4NiuB9DK4drpWuB1rUnLK00qoAg";
     let token_data = zion_aa::utils::decode_jwt(token)?;
 
     let data = json!({
@@ -115,39 +113,49 @@ pub async fn init_contract_wallet(
         "proof": {
             "protocol": "groth16",
             "pi_a": [
-                "14801816815982897191214849187901646186557653634630976994621098935299565837678",
-                "3107512675410124980504736542732965398118402483813760832504749775336079809255",
+                "848517426970185902836957632039262920819670960348695817755692514778205507177",
+                "11234122044483938094114577390707656040020989890470535128512942449808019728442",
                 "1"
             ],
             "pi_b": [
-                [
-                    "15206008218501371535230496796318291276435425082329694810376753231329964445333",
-                    "12979529397566891189029734629963374195985491101285244255488826680679659512201"
-                ],
-                [
-                    "10785241355847769928761783282267618023354984164670580353019210034647839861320",
-                    "15204461572087244143185958420961060931649571485574934594464139847767949860674"
-                ],
-                [
-                    "1",
-                    "0"
-                ]
+                {
+                    "values": [
+                        "2781913689848125025037491843906790085846254178558810253837255454141446402807",
+                        "3647356397285913093210792537100269613673081645711183459275916670275549687181"
+                    ]
+                },
+                {
+                    "values": [
+                        "6057743492438896040617107135360846068647417847934794073591042834408986579993",
+                        "5161670272154989331230524784575966114299788842302085249695831517064950839407"
+                    ]
+                },
+                {
+                    "values": [
+                        "1",
+                        "0"
+                    ]
+                }
             ],
             "pi_c": [
-                "17810260805960448247673615359179766731050272460642672129728519214520556494701",
-                "16900961044965525423879885912038078627949256248409680923890979761253678529942",
+                "11454890441803773990669541648478857444364496620561490584390018845816952078066",
+                "11602134405069161074303940861377076534746484968292843878270724121787321720399",
                 "1"
             ]
         },
-        "ephemeral_key_pair": "e8e7c8b1aaf0398854ee51eb72eed6dd6c90e21128ee60010fda6ea18f691a87",
+        "ephemeral_key_pair": "6ee8dfb5119ac9915cb2a90de1be47f207a780cb09a3355981318a328991cd1e",
         "beneficiaries": [
             "0xfe39693d77c7c83e26ff7df39c13fc36f9cc88f5"
         ]
     });
-    let login_data = serde_json::from_value::<LoginData>(data).unwrap();
+    let AuthorizationData {
+        salt,
+        proof,
+        ephemeral_key_pair,
+        beneficiaries,
+    } = serde_json::from_value::<AuthorizationData>(data).unwrap();
 
-    let address_beneficiaries = login_data
-        .beneficiaries
+    let address_beneficiaries = beneficiaries
         .iter()
         .map(|b| b.parse::<Address>().unwrap())
         .collect::<Vec<_>>();
@@ -160,7 +168,7 @@ pub async fn init_contract_wallet(
         ZionClient::try_new(
             rpc_endpoint,
             contract_wallet_operator.chain_id,
-            &login_data.ephemeral_key_pair,
+            &ephemeral_key_pair,
         )
         .await?,
     );
@@ -170,7 +178,8 @@ pub async fn init_contract_wallet(
     //     ephemeral_key_pair,
     //     beneficiaries,
     // };
-    let jwt_options = JWTOptions::<LocalWallet>::try_init(token_data, login_data)?;
+    let jwt_options =
+        JWTOptions::<LocalWallet>::try_init(token_data, ephemeral_key_pair, proof, salt)?;
 
     let operator = Arc::new(Operator::new(
         contract_wallet_operator,
