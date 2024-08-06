6d74658fbf2193401fd8fbb7bd49be0a25dd55b3
dinhnq-gihot <dinh.nguyen@gihot.vn>
2024-08-06 16:20:09 +0700

    chore: remove client and use sdk client instead

---
+++ openapi/openapi-ethers/src/provider.rs
@@ -0,0 +1,19 @@
+use {
+    super::config::Config,
+    anyhow::Result,
+    ethers::providers::Provider,
+    ethers_providers::{Http, ProviderExt},
+    std::sync::Arc,
+};
+
+pub async fn init_provider(config: Config) -> Result<(Arc<Provider<Http>>, Arc<Provider<Http>>)> {
+    let Config {
+        zion_rpc,
+        torii_rpc,
+        chain_id,
+    } = config;
+    let zion_provider = Arc::new(Provider::<Http>::connect(zion_rpc.as_str()).await);
+    let torii_provider = Arc::new(Provider::<Http>::connect(torii_rpc.as_str()).await);
+
+    Ok((zion_provider, torii_provider))
+}
